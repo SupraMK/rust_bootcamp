181b731378c7aa9fd94708470c858968ee49969d
Guillaume <guillaume.sinamapro@gmail.com>
2026-01-14 14:37:30 +0100

    Add hello_world

---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/supra/Library/CloudStorage/GoogleDrive-guillaume.sinama@gmail.com/Mon\ Drive/Rust/rust_bootcamp/hello_world/target/debug/hello_world: /Users/supra/Library/CloudStorage/GoogleDrive-guillaume.sinama@gmail.com/Mon\ Drive/Rust/rust_bootcamp/hello_world/src/main.rs
